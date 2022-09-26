cb305c4eff829bd492b42d2eedee781f891f074f
Abhisek Das <92661441+dasabhisek96@users.noreply.github.com>
2022-09-26 19:42:15 +0530

    Initial commit

---
+++ routes/aliens.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const router = express.Router()
+const Alien = require('../models/alien')
+router.get('/', async(req,res) => {
+    try{
+             const aliens = await Alien.find() 
+             res.json(aliens)
+    }catch(err) {
+        res.send('Error ' +err)
+    }
+    
+})
+
+router.get('/:id', async(req,res) => {
+    try{
+             const alien = await Alien.findById(req.params.id) 
+             res.json(alien)
+    }catch(err) {
+        res.send('Error ' +err)
+    }
+    
+})
+
+router.post('/', async(req,res) => {
+    console.log(req.body)
+    const alien = new Alien({
+        name: req.body.name,
+        tech: req.body.tech,
+        sub: req.body.sub
+    })
+    try {
+        const a1 = await alien.save()
+        res.json(a1)
+
+    }catch(err){
+        console.log(err)
+        res.send(err )
+    }
+})
+
+router.patch('/:d',async(req,res)=> {
+    console.log(req.body)
+    try{
+         const alien = await Alien.findById(req.params.id)
+         alien.sub = req.body.sub
+         const a1 = await alien.save()
+         res.json(a1)
+    }catch(err){
+        console.log(err)
+        res.send(err)
+    }
+})
+
+module.exports = router
